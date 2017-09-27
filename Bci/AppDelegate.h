e829dacf0874f4d91662d3a79762033447d213ca
Gabriel Camarena Gomez <gcamarenagomez@gcamarenag-ltm1.local>
2017-09-27 08:44:59 -0500

    Initial Commit

---
+++ Bci/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bci
+//
+//  Created by Gabriel Camarena Gomez on 9/27/17.
+//  Copyright © 2017 Salesforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bci/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bci
+//
+//  Created by Gabriel Camarena Gomez on 9/27/17.
+//  Copyright © 2017 Salesforce. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
